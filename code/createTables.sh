0f73ca88be51218b7f2c44734ebff43590fcd158
Joshua Bloom <jbloom@mednet.ucla.edu>
2020-07-05 14:52:26 -0700

    bash script to generate count tables for each run

---
+++ code/createTables.sh
@@ -0,0 +1,19 @@
+#populate each run directory with XLSX file describing run or CSV sample sheet and then run the following
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v7/ 195116944 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v8/ 195154960 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v9/ 195184025 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v10/ 195218041  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v11/ 195336198 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v12/ 195340204 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v13/ 195360215  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v14/ 195435293  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v15/ 195481337  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v16/ 195535379 TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v17/ 195853687  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v18/ 195886700  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v19/ 195934811  TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v20/ 196002849  TRUE 
+#nextseq run at TCGB core
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v21/ NULL TRUE 
+Rscript --vanilla /data/Covid/swabseq/code/countAmplicons.R /data/Covid/swabseq/runs/v22/ 196030855 TRUE 
+
