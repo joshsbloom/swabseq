2c29b983894ce7fb2a77b6bee718c25edbbeb70a
Joshua Bloom <jbloom@mednet.ucla.edu>
2020-09-04 19:42:01 -0700

    v41 updates

--- analysis/v45/analysis.R
+++ analysis/v45/analysis.R
@@ -1,8 +1,8 @@
 swabseq.dir="/mnt/e/swabseq/"
 swabseq.dir="/data/Covid/swabseq/"
 source(paste0(swabseq.dir, 'code/helper_functions.R'))
-rundir=paste0(swabseq.dir, 'runs/v43/')
-outdir=paste0(swabseq.dir, 'analysis/v43/')
+rundir=paste0(swabseq.dir, 'runs/v45/')
+outdir=paste0(swabseq.dir, 'analysis/v45/')
 
 dfL=mungeTables(paste0(rundir, 'countTable.RDS'),lw=T, Stotal_filt=500, input=384)
 df=dfL$df
@@ -11,7 +11,7 @@ dfs=dfL$dfs
 #sum(df$Count)
 #17477902
 
-titl='v43 - 1536 UDI'
+titl='v45 - rerun of v43 rapid run miniseq 1536 UDI plate 2 only'
 
 #plate visualization 
 #plate visualization 
@@ -23,17 +23,17 @@ df %>%
   scale_fill_viridis_c(option = 'plasma')+ggtitle(titl)
 ggsave(paste0(outdir,'plateVis_all_indices.png'))
 
-filled.plates=add384Mapping(df)
-filled.plates %>% 
-    ggplot(aes(x=Col384, y=Row384, fill=log10(Count))) + 
-  geom_raster() +
-  coord_equal() +
-  facet_grid(amplicon~Plate_384) +
-  scale_fill_viridis_c(option = 'plasma')+ggtitle(titl)
-ggsave(paste0(outdir,'plateVis_384.png'))
+#filled.plates=add384Mapping(df)
+#filled.plates %>% 
+#    ggplot(aes(x=Col384, y=Row384, fill=log10(Count))) + 
+#  geom_raster() +
+#  coord_equal() +
+#  facet_grid(amplicon~Plate_384) +
+#  scale_fill_viridis_c(option = 'plasma')+ggtitle(titl)
+#ggsave(paste0(outdir,'plateVis_384.png'))
 
 
-df %>% filter(Description!='' & Description!=' ') %>% #filter(Plate_ID!='Plate1') %>%
+df %>% filter(Plate_ID=='Plate2') %>% #filter(Plate_ID!='Plate1') %>%
     ggplot(aes(x=Col, y=Row, fill=log10(Count))) + 
   geom_raster() +
   coord_equal() +
